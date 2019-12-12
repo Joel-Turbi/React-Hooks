e4596cdc8fe945e52b288fbb27242d3f52ac5337
Joel <20152521@itla.edu.do>
2019-12-12 10:56:35 -0400

    hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import { Card, Button, Col, Row } from 'antd';
 import './App.css';
 
 function App() {
+  
+  const [count,setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Card  bordered={false} style={{ width: 300 }}>
+
+    <Row type="flex" justify="center" align="middle">
+      <Col span={8}> <Button type="primary" shape="circle" icon="plus" onClick={()=>setCount(count+1)} /></Col>
+  <Col span={8}><h2>{count}</h2></Col>
+      <Col span={8}><Button type="primary" shape="circle" icon="minus" onClick={()=>setCount(count-1)} /></Col>
+    </Row>
+  </Card>
+
   );
 }
 
